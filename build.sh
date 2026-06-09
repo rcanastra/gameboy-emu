c521d6167e19949cc216a56ad3e528d131af5d7c
Ryoma Canastra <ryoma.canastra@gmail.com>
2026-06-09 10:29:32 -0700

    better comments

--- build.sh
+++ build.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-# From your repo/ directory
 # g++-15 is just to pin a version, and it's what i had
 cmake -B build -S . -D CMAKE_CXX_COMPILER=g++-15 -D CMAKE_TOOLCHAIN_FILE=$VCPKG_ROOT/scripts/buildsystems/vcpkg.cmake
 
-# Compile
 cmake --build build
